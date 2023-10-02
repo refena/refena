f5a4edb413f47763f3b70627615881293447f526
Tien Do Nam <dev.tien.donam@gmail.com>
2023-10-02 02:59:53 +0200

    feat: add inspector

---
+++ refena_inspector/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyPage'),
+      ),
+      body: const Center(
+        child: Text('Refena Inspector'),
+      ),
+    );
+  }
+}
