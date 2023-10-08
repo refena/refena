0d8a3cfd0000e18105353f34418c62981192e0ed
Tien Do Nam <dev.tien.donam@gmail.com>
2023-10-09 00:12:41 +0200

    feat(inspector): cache compiled app

--- refena_inspector/bin/refena_inspector.dart
+++ refena_inspector/bin/refena_inspector.dart
@@ -6,10 +6,36 @@ import 'package:path/path.dart' as p;
 import 'package:refena_inspector/util/logger.dart';
 
 final _logger = Logger('RefenaInspectorBoot');
-const _inspectorPath = '.refena_inspector';
+const _compilePath = '.refena_inspector/compile';
+const _appPath = '.refena_inspector/app';
 
 void main() async {
   initLogger();
+
+  // Run compiled app if exists
+  if (Directory(_appPath).existsSync()) {
+    _logger.info('Detected compiled app. Running...');
+    if (Platform.isWindows) {
+      Process.runSync(
+        '$_appPath\\refena_inspector.exe',
+        [],
+        runInShell: true,
+      );
+    } else if (Platform.isMacOS) {
+      Process.runSync(
+        'open',
+        ['$_appPath/refena_inspector.app'],
+        runInShell: true,
+      );
+    } else {
+      Process.runSync(
+        '$_appPath/refena_inspector',
+        [],
+        runInShell: true,
+      );
+    }
+    return;
+  }
 
   final config = Platform.packageConfig;
   if (config == null) {
@@ -36,18 +62,32 @@ void main() async {
     ));
   }
   _logger.info('RefenaInspector Path: $refenaPath');
-  _logger.info('Temp Path: $_inspectorPath');
+
+  // Create compile path
+  if (Platform.isWindows) {
+    Process.runSync(
+      'mkdir',
+      [_compilePath],
+      runInShell: true,
+    );
+  } else {
+    Process.runSync(
+      'mkdir',
+      ['-p', _compilePath],
+      runInShell: true,
+    );
+  }
 
   if (Platform.isWindows) {
     Process.runSync(
       'xcopy',
-      ['/e', '/k', '/h', '/i', refenaPath, _inspectorPath],
+      ['/e', '/k', '/h', '/i', refenaPath, _compilePath],
       runInShell: true,
     );
   } else {
     Process.runSync(
       'cp',
-      ['-R', refenaPath, _inspectorPath],
+      ['-R', '$refenaPath/', _compilePath],
       runInShell: true,
     );
   }
@@ -67,12 +107,105 @@ void main() async {
           ? 'macos'
           : 'linux';
 
+  // Compile app
+  _logger.info('Compiling app in $_compilePath');
   final process = await Process.start(
     flutterPath,
-    ['run', '-d', device, '--profile'],
+    ['build', device],
     mode: ProcessStartMode.inheritStdio,
-    workingDirectory: _inspectorPath,
+    workingDirectory: _compilePath,
     runInShell: true,
   );
   await process.exitCode;
+
+  // Create app path
+  _logger.info('Creating app in $_appPath');
+  if (Platform.isWindows) {
+    Process.runSync(
+      'mkdir',
+      [_appPath],
+      runInShell: true,
+    );
+  } else {
+    Process.runSync(
+      'mkdir',
+      ['-p', _appPath],
+      runInShell: true,
+    );
+  }
+
+  // Copy to app path
+  _logger.info('Copying app to $_appPath');
+  if (Platform.isWindows) {
+    Process.runSync(
+      'xcopy',
+      [
+        '/e',
+        '/k',
+        '/h',
+        '/i',
+        '$_compilePath\\build\\$device\\runner\\Release',
+        _appPath,
+      ],
+      runInShell: true,
+    );
+  } else if (Platform.isMacOS) {
+    Process.runSync(
+      'cp',
+      [
+        '-R',
+        '$_compilePath/build/macos/Build/Products/Release/refena_inspector.app',
+        _appPath,
+      ],
+      runInShell: true,
+    );
+  } else {
+    Process.runSync(
+      'cp',
+      [
+        '-R',
+        '$_compilePath/build/$device/release/bundle/libexec/refena_inspector',
+        _appPath,
+      ],
+      runInShell: true,
+    );
+  }
+
+  // Remove compile path
+  _logger.info('Removing $_compilePath');
+  if (Platform.isWindows) {
+    Process.runSync(
+      'rmdir',
+      ['/s', '/q', _compilePath],
+      runInShell: true,
+    );
+  } else {
+    Process.runSync(
+      'rm',
+      ['-rf', _compilePath],
+      runInShell: true,
+    );
+  }
+
+  // Run
+  _logger.info('Running app');
+  if (Platform.isWindows) {
+    Process.runSync(
+      '$_appPath\\refena_inspector.exe',
+      [],
+      runInShell: true,
+    );
+  } else if (Platform.isMacOS) {
+    Process.runSync(
+      'open',
+      ['$_appPath/refena_inspector.app'],
+      runInShell: true,
+    );
+  } else {
+    Process.runSync(
+      '$_appPath/refena_inspector',
+      [],
+      runInShell: true,
+    );
+  }
 }
