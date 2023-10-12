db413d1925bb34b0da2ab743b6099ef8f9d0718a
Tien Do Nam <dev.tien.donam@gmail.com>
2023-10-12 23:14:20 +0200

    feat(inspector): open tracing page in debug mode

--- refena_inspector/lib/pages/home/settings_page.dart
+++ refena_inspector/lib/pages/home/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:refena_flutter/refena_flutter.dart';
 import 'package:refena_inspector/service/settings_service.dart';
@@ -19,32 +20,44 @@ class SettingsPage extends StatelessWidget {
           child: ListView(
             children: [
               _SettingsEntry(
-                  label: 'Theme',
-                  child: DropdownButton<ThemeMode>(
-                    value: settingsState.themeMode,
-                    onChanged: (value) {
-                      if (value == null) {
-                        return;
-                      }
-                      ref
-                          .redux(settingsProvider)
-                          .dispatch(SettingsThemeModeAction(themeMode: value));
+                label: 'Theme',
+                child: DropdownButton<ThemeMode>(
+                  value: settingsState.themeMode,
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    ref
+                        .redux(settingsProvider)
+                        .dispatch(SettingsThemeModeAction(themeMode: value));
+                  },
+                  items: const [
+                    DropdownMenuItem(
+                      value: ThemeMode.system,
+                      child: Text('System'),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.light,
+                      child: Text('Light'),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.dark,
+                      child: Text('Dark'),
+                    ),
+                  ],
+                ),
+              ),
+              if (kDebugMode)
+                _SettingsEntry(
+                  label: 'Tracing',
+                  child: FilledButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => const RefenaTracingPage()));
                     },
-                    items: const [
-                      DropdownMenuItem(
-                        value: ThemeMode.system,
-                        child: Text('System'),
-                      ),
-                      DropdownMenuItem(
-                        value: ThemeMode.light,
-                        child: Text('Light'),
-                      ),
-                      DropdownMenuItem(
-                        value: ThemeMode.dark,
-                        child: Text('Dark'),
-                      ),
-                    ],
-                  )),
+                    child: const Text('Open'),
+                  ),
+                ),
             ],
           ),
         ),
